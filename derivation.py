42ce07d4161feaf877ba910469fda5c9877988e4
yinyingyi <1041739799@qq.com>
2020-05-05 00:08:40 +0800

    python实战二作业四：列表推导式

--- derivation.py
+++ derivation.py
@@ -2,8 +2,7 @@
 给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序
 条件：（1）要有for循环；（2）要有append
 '''
-#if list_a[i] > list_a[i+1]
-    #list_a[i],list_a[i+1] = list_a[i+1],list_a[i]
+
 # 方法一：for循环
 # list_a = [2,8,4,-25,16]
 # list_b = []
